d209e026f6c95a999685122469e0218f6d6c1119
kchan <kwunfuchan@gmail.com>
2024-01-06 22:38:59 +0000

    [client] revise scripts - fixed resize dis-position

--- client/public/scripts/modules/resizing.js
+++ client/public/scripts/modules/resizing.js
@@ -22,28 +22,24 @@ function resizing() {
       console.log(index_about_fixedBox.style.left);
     }
 
-    // 2b. Fixed Resize Differentiation
-    if (
-      // (i) for the frame container
-      window.scrollY >= length.index_about_top &&
-      window.scrollY < length.index_projects_top
-    ) {
+
+    // 2a. Fixed Resize Differentiation
+    // (1) less than about section
+    if (window.scrollY < length.index_about_top && window.innerWidth >= length.breakpoint_large) {
+      index_about_fixedBox.style.transform = "translateX(-220px)";
+    }
+    else if (window.scrollY < length.index_about_top && window.innerWidth < length.breakpoint_large) {
+      index_about_fixedBox.style.transform = "translateX(0px)";
+    }
+    // (2) within about section
+    if (window.scrollY >= length.index_about_top && window.scrollY < length.index_projects_top) {
       index_about_fixedBox.style.position = "fixed";
       index_about_scroll.style.transition = `transform 1s ease-in-out`;
-
-      // (ii) for the slides settings
-      if (window.scrollY < length.index_about_top) {
-        window.innerWidth > length.breakpoint_large ?
-          index_about_scroll.style.transform = `translateX(${(window.innerWidth - length.overlays_width) * 0}px)` :
-          index_about_scroll.style.transform = `translateX(${window.innerWidth * 0}px)`;
-      }
-      else if (window.scrollY < length.index_projects_top) {
-        window.innerWidth > length.breakpoint_large ?
-          index_about_scroll.style.transform = `translateX(${(window.innerWidth - length.overlays_width) * -1}px)` :
-          index_about_scroll.style.transform = `translateX(${window.innerWidth * -1}px)`;
-      }
     }
+
+
   });
+
 }
 
 // EXPORT
