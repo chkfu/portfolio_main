36b2e7a6918d8ca4a6267e58864e0b5f627ef873
kchan <kwunfuchan@gmail.com>
2024-01-07 00:50:24 +0000

    [client] revise scripts - fixed resize dis-position (2)

--- client/public/scripts/modules/resizing.js
+++ client/public/scripts/modules/resizing.js
@@ -7,12 +7,16 @@ function resizing() {
   // 1. Declarations
   // 1a. Index About Section Tags
   const index_about_fixedBox = document.getElementById("about_fixed_box");
-  const index_about_scroll = document.getElementById("about_absolute_scroll");
-
 
   // 2. About Section Resizing
   window.addEventListener("resize", () => {
     // 2a. Frame position change
+    /* 
+      Remarks: 
+      - "index_about_fixedBox.style.left" avoid overlapping with underlay background
+      - Adjusting inner_about_scroll to specific length will lead to mis-positioning, after resizing
+      - BEST SOLUTION: Overlapping now only exist between hero and about sections, can be solved by scrolling
+    */
     if (window.innerWidth > length.breakpoint_large) {
       index_about_fixedBox.style.left = `${length.overlays_width}px`;
       console.log(index_about_fixedBox.style.left);
@@ -21,25 +25,7 @@ function resizing() {
       index_about_fixedBox.style.left = "0px";
       console.log(index_about_fixedBox.style.left);
     }
-
-
-    // 2a. Fixed Resize Differentiation
-    // (1) less than about section
-    if (window.scrollY < length.index_about_top && window.innerWidth >= length.breakpoint_large) {
-      index_about_fixedBox.style.transform = "translateX(-220px)";
-    }
-    else if (window.scrollY < length.index_about_top && window.innerWidth < length.breakpoint_large) {
-      index_about_fixedBox.style.transform = "translateX(0px)";
-    }
-    // (2) within about section
-    if (window.scrollY >= length.index_about_top && window.scrollY < length.index_projects_top) {
-      index_about_fixedBox.style.position = "fixed";
-      index_about_scroll.style.transition = `transform 1s ease-in-out`;
-    }
-
-
   });
-
 }
 
 // EXPORT
