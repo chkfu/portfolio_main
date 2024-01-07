e1c27f8a62871830459476a228e2f9b3d035bac8
kchan <kwunfuchan@gmail.com>
2024-01-07 23:02:20 +0000

    [client] build about section slides content

--- client/public/scripts/modules/scrolling.js
+++ client/public/scripts/modules/scrolling.js
@@ -18,6 +18,9 @@ function scrolling() {
   const index_about_fixedBox = document.getElementById("about_fixed_box");
   const index_about_scroll = document.getElementById("about_absolute_scroll");
   const inner_about_slides = document.querySelectorAll(".about_slides");
+  const inner_about_poster02 = document.getElementById("poster_backend");
+  // 1d. Index Project Section Tags
+  const index_project_container = document.getElementById("index_section_projects");
 
 
   // 2. Navigation Bar Scrolling
@@ -67,7 +70,6 @@ function scrolling() {
       index_heading_contact.style.display = "none";
     }
     else if (window.scrollY > length.index_projects_top && window.scrollY <= length.index_contact_top) {
-      console.log('testing');
       index_heading_portfolio.style.display = "none";
       index_heading_about.style.display = "none";
       index_heading_projects.style.display = "flex";
@@ -139,6 +141,20 @@ function scrolling() {
       index_about_fixedBox.style.left = "0px";
     }
   });
+
+
+  // 5. Index Projects Scrolling
+  window.addEventListener("scroll", () => {
+    if (index_project_container.getBoundingClientRect().y <= 768 + 40 && window.innerWidth >= 1024) {
+      inner_about_poster02.style.transition = "all 0.5s";
+      inner_about_poster02.style.transform = ` translateX(-220px) `;
+    }
+    else {
+      inner_about_poster02.style.transition = "all 0.5s";
+      inner_about_poster02.style.transform = ` translateX(0px) `;
+    }
+  });
+
 };
 
 
