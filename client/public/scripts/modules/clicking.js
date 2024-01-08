0e299f50d163256c5db417121a72935e6f02f8fd
kchan <kwunfuchan@gmail.com>
2024-01-08 14:41:26 +0000

    [client] revise scripts - fixed sync actions for highlighter clicking

--- client/public/scripts/modules/clicking.js
+++ client/public/scripts/modules/clicking.js
@@ -8,33 +8,51 @@ function clicking() {
   let highlight_status = "skills";
   // 1a. Index About Section Tags
   const index_about_highlighter = document.querySelectorAll(".icon_highlighter");
+  const about_highlight_trigger_01 = document.getElementById("about_highlight_trigger01");
+  const about_highlight_trigger_02 = document.getElementById("about_highlight_trigger02");
   const index_skills_text = document.querySelectorAll("span.text_gp01");
   const index_techs_text = document.querySelectorAll("span.text_gp02");
 
   // 2. About Section Resizing
   window.addEventListener("click", () => {
-    index_about_highlighter.forEach((trigger) => {
-      trigger.style.setProperty("--after-color", "#4dabf7");
+    index_about_highlighter.forEach(() => {
+      // 2a. swapping icon text
       if (highlight_status === "skills") {
-        trigger.setAttribute("data-highlight", "techs");
-        trigger.style.setProperty("--after-color", "#fcc419");
-        index_skills_text.forEach((text) => text.style.color = "#dddddd");
-        index_techs_text.forEach((text) => text.style.color = "#fcc419");
         highlight_status = "techs";
       }
       else if (highlight_status === "techs") {
-        trigger.setAttribute("data-highlight", "none");
-        trigger.style.setProperty("--after-color", "#999999");
-        index_skills_text.forEach((text) => text.style.color = "#dddddd");
-        index_techs_text.forEach((text) => text.style.color = "#dddddd");
         highlight_status = "none";
       }
       else if (highlight_status === "none") {
-        trigger.setAttribute("data-highlight", "skills");
-        trigger.style.setProperty("--after-color", "#4dabf7");
+        highlight_status = "skills";
+      }
+
+      // 2b. Operating styling effects
+      if (highlight_status === "skills") {
+        // # Remarks: "trigger" only updated the clicked icon, another one will be skipped 
+        about_highlight_trigger_01.setAttribute("data-highlight", "skills");
+        about_highlight_trigger_01.style.setProperty("--after-color", "#4dabf7");
+        about_highlight_trigger_02.setAttribute("data-highlight", "skills");
+        about_highlight_trigger_02.style.setProperty("--after-color", "#4dabf7");
         index_skills_text.forEach((text) => text.style.color = "#4dabf7");
         index_techs_text.forEach((text) => text.style.color = "#dddddd");
-        highlight_status = "skills";
+      }
+
+      else if (highlight_status === "techs") {
+        about_highlight_trigger_01.setAttribute("data-highlight", "techs");
+        about_highlight_trigger_01.style.setProperty("--after-color", "#fcc419");
+        about_highlight_trigger_02.setAttribute("data-highlight", "techs");
+        about_highlight_trigger_02.style.setProperty("--after-color", "#fcc419");
+        index_skills_text.forEach((text) => text.style.color = "#dddddd");
+        index_techs_text.forEach((text) => text.style.color = "#fcc419");
+      }
+      else if (highlight_status === "none") {
+        about_highlight_trigger_01.setAttribute("data-highlight", "none");
+        about_highlight_trigger_01.style.setProperty("--after-color", "#dddddd");
+        about_highlight_trigger_02.setAttribute("data-highlight", "none");
+        about_highlight_trigger_02.style.setProperty("--after-color", "#dddddd");
+        index_skills_text.forEach((text) => text.style.color = "#dddddd");
+        index_techs_text.forEach((text) => text.style.color = "#dddddd");
       }
     });
   });
